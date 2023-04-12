b2b302f25f83777c996a8fd16d62fcb5a3925841
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-12 16:06:45 +0800

    add comments and v1 functions

--- contracts/core/UniversalLiquidatorRegistry.sol
+++ contracts/core/UniversalLiquidatorRegistry.sol
@@ -2,13 +2,17 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
+// imported contracts and libraries
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import {ULRegistryStorage} from "./storage/Storage.sol";
-
+// interfaces
 import "../interface/IUniversalLiquidatorRegistry.sol";
 
+// librarise
 import "../libraries/DataTypes.sol";
+
+// constants and types
+import {ULRegistryStorage} from "./storage/Storage.sol";
 
 contract UniversalLiquidatorRegistry is
     Ownable,
@@ -71,5 +75,49 @@ contract UniversalLiquidatorRegistry is
 
         // path can also be empty
         paths[_sellToken][_buyToken] = DataTypes.PathInfo(_dex, _paths);
+    }
+
+    function addIntermediateToken(address _token) public override onlyOwner {
+        intermediateTokens.push(_token);
+    }
+
+    function addDex(bytes32 _name, address _dex) public override onlyOwner {
+        require(!_dexExists(_name), "Dex already exists");
+        dexesInfo[_name] = _dex;
+        allDexes.push(_name);
+    }
+
+    function changeDexAddress(
+        bytes32 _name,
+        address _dex
+    ) public override onlyOwner {
+        require(_dexExists(_name), "Dex does not exists");
+        dexesInfo[_name] = _dex;
+    }
+
+    function getAllDexes() public view override returns (bytes32[] memory) {
+        uint256 totalDexes = 0;
+
+        for (uint256 idx = 0; idx < allDexes.length; idx++) {
+            if (dexesInfo[allDexes[idx]] != address(0)) {
+                totalDexes++;
+            }
+        }
+
+        bytes32[] memory retDexes = new bytes32[](totalDexes);
+        uint256 retIdx = 0;
+
+        for (uint256 idx = 0; idx < allDexes.length; idx++) {
+            if (dexesInfo[allDexes[idx]] != address(0)) {
+                retDexes[retIdx] = allDexes[idx];
+                retIdx++;
+            }
+        }
+
+        return retDexes;
+    }
+
+    function _dexExists(bytes32 _name) internal view returns (bool) {
+        return dexesInfo[_name] != address(0);
     }
 }
