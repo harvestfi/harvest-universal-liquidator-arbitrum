4f60ae2a0185ff983e1983d353e48c5ef38d400b
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-14 14:37:02 +0800

    remove unuse file

--- contracts/libraries/Dexes.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
-
-library Dexes {
-    bytes32 public constant uniDex = bytes32(uint256(keccak256("uni")));
-    bytes32 public constant sushiDex = bytes32(uint256(keccak256("sushi")));
-}
