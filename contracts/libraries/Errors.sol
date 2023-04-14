eb8157b3425f8b4bcc637545e37fabb8d450f867
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-14 15:04:16 +0800

    add errors library

---
+++ contracts/libraries/Errors.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+library Errors {
+    error InvalidPathsStart();
+    error InvalidPathsEnd();
+    error DexExists();
+    error DexDoesNotExist();
+
+    error AmountUnmatch();
+}
