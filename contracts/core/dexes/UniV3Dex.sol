4c2d1ce2a3420bcd08867ed3512f4f52ce653034
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-15 23:43:14 +0800

    Remove configuration code to improve readability - Remove constructor and use script to add pairFee in the future - Remove the router constant address and import address from libraries

--- contracts/core/dexes/UniV3Dex.sol
+++ contracts/core/dexes/UniV3Dex.sol
@@ -1,33 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+// imported contracts and libraries
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+// interfaces
 import "../../interface/ILiquidityDex.sol";
 import "../../interface/uniswap/ISwapRouter.sol";
+
+// libraries
+import "../../libraries/Addresses.sol";
 
 contract UniV3Dex is ILiquidityDex, Ownable {
     using SafeERC20 for IERC20;
 
-    address constant uniswapV3Router =
-        0xE592427A0AEce92De3Edee1F18E0157C05861564;
-
     mapping(address => mapping(address => uint24)) public storedPairFee;
-
-    constructor() public {
-        address fcash = 0x531261a091F31bFd93dd393a6CA447ed6Fb2043C;
-        address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-        address usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-        address eurs = 0xdB25f211AB05b1c97D595516F45794528a807ad8;
-        storedPairFee[dai][usdc] = 500;
-        storedPairFee[dai][usdt] = 500;
-        storedPairFee[usdc][usdt] = 500;
-        storedPairFee[fcash][usdc] = 500;
-        storedPairFee[usdc][eurs] = 500;
-    }
 
     function doSwap(
         uint256 amountIn,
@@ -44,7 +33,7 @@ contract UniV3Dex is ILiquidityDex, Ownable {
             amountIn
         );
         IERC20(currentSellToken).safeIncreaseAllowance(
-            uniswapV3Router,
+            Addresses.uniswapV3Router,
             amountIn
         );
 
@@ -68,9 +57,8 @@ contract UniV3Dex is ILiquidityDex, Ownable {
                 amountOutMinimum: minAmountOut
             });
 
-        uint256 actualAmountOut = ISwapRouter(uniswapV3Router).exactInput(
-            param
-        );
+        uint256 actualAmountOut = ISwapRouter(Addresses.uniswapV3Router)
+            .exactInput(param);
         return actualAmountOut;
     }
 
