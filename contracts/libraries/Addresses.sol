aaed83d0fdfd3ddfb9453afe43a769a51252a921
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-20 14:09:38 +0800

    Add new constants for new dexes

--- contracts/libraries/Addresses.sol
+++ contracts/libraries/Addresses.sol
@@ -4,4 +4,6 @@ pragma solidity 0.8.17;
 library Addresses {
     address constant uniswapV3Router =
         0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    address constant balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
 }
