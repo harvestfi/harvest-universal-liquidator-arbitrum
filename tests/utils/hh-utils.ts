6a2bb49d6660faddf46a4f8e24d8fb8285114426
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-28 00:06:54 +0800

    style: remove logs

--- tests/utils/hh-utils.ts
+++ tests/utils/hh-utils.ts
@@ -1,9 +1,7 @@
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 
 export async function impersonates(targetAccounts: string[]) {
-  console.log("Impersonating...");
   for (let i = 0; i < targetAccounts.length; i++) {
-    console.log(targetAccounts[i]);
     await impersonateAccount(targetAccounts[i]);
   }
 }
