42846ca20d34bed7a8d366b1b46cfe804ac0e228
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-28 00:09:47 +0800

    build: fix system setup to isolate the swap test, otherwise, swaps will affect each other with the balance check

--- tests/system.test.ts
+++ tests/system.test.ts
@@ -1,61 +1,74 @@
 // Utilities
 import addresses from "../helpers/addresses.json";
+import dex from "../helpers/dexes.json";
+import poolIds from "../helpers/poolIds.json";
 import tokenPairs from "../helpers/token-pairs.json";
 import intermediateTokens from "../helpers/intermediate-tokens.json";
 import * as utils from "./utils";
 
-import { contracts } from "../build/typechain";
-
 import { ethers } from "hardhat";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("All System", function () {
-  let accounts: SignerWithAddress[];
-
-  // external accounts
-  let testFarmer: SignerWithAddress;
-  let governance: SignerWithAddress;
-  let registry: contracts.core.UniversalLiquidatorRegistry;
-  let universalLiquidator: contracts.core.UniversalLiquidator;
-
-  before(async function () {
-    accounts = await ethers.getSigners();
-    testFarmer = accounts[1];
+  async function deploySetup() {
+    const accounts = await ethers.getSigners();
+    const testFarmer = accounts[1];
 
     // impersonate accounts
     await utils.impersonates([addresses.Governance]);
-    governance = await ethers.getSigner(addresses.Governance);
+    const governance = await ethers.getSigner(addresses.Governance);
 
     await accounts[9].sendTransaction({
       to: governance.address,
       value: ethers.utils.parseEther("5") // 5 ether
     })
 
+    // deploy contracts
     const contracts = await utils.setupSystem(governance);
-    registry = contracts.registry;
-    universalLiquidator = contracts.universalLiquidator;
-  });
+    const registry = contracts.registry;
+    const universalLiquidator = contracts.universalLiquidator;
+    const deployedDexes = contracts.deployedDexes;
 
-  describe("Happy path", function () {
-    it("Add new intermediate token to ULRegistry", async function () {
-      const list = intermediateTokens.list.map((token) => {
-        return token.address;
+    // add intermediate tokens
+    const intermediateTokensList = intermediateTokens.list.map((token) => {
+      return token.address;
+    });
+    await utils.updateIntermediateToken(intermediateTokensList, registry, governance);
+
+    // add pools
+    poolIds.list.forEach(async (poolIds) => {
+      const dexName = poolIds.name;
+      const deployedDex = deployedDexes.find((dex) => dex?.name === dexName);
+      const dexFile = dex.list.find((dex) => dex?.name === dexName);
+      if (!deployedDex) {
+        throw new Error(`Could not find dex with name ${dexName}`);
+      }
+      if (!dexFile) {
+        throw new Error(`Could not find contract file with name ${dexName}`);
+      }
+      const dexContract = await ethers.getContractAt(dexFile.file, deployedDex.address);
+      poolIds.pools.forEach(async (poolId) => {
+        await utils.addPoolIds(poolId, dexContract, governance);
       });
-      await utils.updateIntermediateToken(list, registry, governance);
     });
 
-    it("Add new path to ULRegistry", async function () {
-      const list = tokenPairs.list;
-      for (const tokenPair of list) {
-        await utils.addPaths(tokenPair, registry, governance);
-      }
-    });
+    // add token pairs
+    const tokenPairList = tokenPairs.list;
+    for (const tokenPair of tokenPairList) {
+      await utils.addPaths(tokenPair, registry, governance);
+    }
 
-    it("Swapping Test Token", async function () {
-      const list = tokenPairs.list;
-      for (const tokenPair of list) {
-        await utils.swapTest(testFarmer, tokenPair, universalLiquidator, accounts[9]);
-      }
-    });
+    return { testFarmer, universalLiquidator, faucet: accounts[9] };
+  }
+
+  describe("Happy Path", function () {
+    const list = tokenPairs.list;
+    for (const tokenPair of list) {
+      it("Swapping Test Token", async function () {
+        const { testFarmer, universalLiquidator, faucet } = await loadFixture(deploySetup);
+        console.log(universalLiquidator.address);
+        await utils.swapTest(testFarmer, tokenPair, universalLiquidator, faucet);
+      });
+    }
   });
 });
