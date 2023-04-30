6f029f4205b7e69526a5dfa86a2bfc3ffa483170
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-30 23:16:57 +0800

    test: build structure of the functionality test of dexes

---
+++ tests/dex.test.ts
@@ -0,0 +1,22 @@
+describe("Dexes: Functionality Tests", function () {
+    describe("Uniswap", function () {
+        describe("Happy Path", function () {
+            it("Set fee", async function () {
+            });
+        });
+        describe("Ownership", function () {
+            it("Only owner can set fee", async function () {
+            });
+        });
+    });
+    describe("Balancer", function () {
+        describe("Happy Path", function () {
+            it("Set poolId", async function () {
+            });
+        });
+        describe("Ownership", function () {
+            it("Only owner can set poolId", async function () {
+            });
+        });
+    });
+});
