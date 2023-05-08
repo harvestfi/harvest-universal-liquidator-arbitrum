a95b1732d001db72465a73cd41f875fb9c7d8ca5
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-05-08 14:00:11 +0800

    fix: add else statement to the router execution
    Add else statement to the doSwap() with LizardDex, otherwise both swap execution would be called

--- contracts/core/dexes/LizardDex.sol
+++ contracts/core/dexes/LizardDex.sol
@@ -10,15 +10,16 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../../interface/ILiquidityDex.sol";
 import "../../interface/lizard/ILizardRouter01.sol";
 
+// libraries
+import "../../libraries/Addresses.sol";
+
 // constants and types
 import {LizardDexStorage} from "../storage/LizardDex.sol";
 
+import "hardhat/console.sol";
+
 contract LizardDex is Ownable, ILiquidityDex, LizardDexStorage {
     using SafeERC20 for IERC20;
-
-    constructor(address _router) {
-        router = _router;
-    }
 
     function doSwap(
         uint256 _sellAmount,
@@ -28,19 +29,22 @@ contract LizardDex is Ownable, ILiquidityDex, LizardDexStorage {
     ) public override returns (uint256) {
         address sellToken = _path[0];
 
-        IERC20(sellToken).safeIncreaseAllowance(router, _sellAmount);
+        IERC20(sellToken).safeIncreaseAllowance(
+            Addresses.lizardRouter,
+            _sellAmount
+        );
 
         ILizardRouter01.Route[] memory routes = new ILizardRouter01.Route[](
             _path.length - 1
         );
 
         for (uint256 idx; idx < _path.length - 1; ) {
-            address sellToken = _path[idx];
-            address buyToken = _path[idx + 1];
+            address curSellToken = _path[idx];
+            address curBuyToken = _path[idx + 1];
 
-            routes[idx].from = sellToken;
-            routes[idx].to = buyToken;
-            routes[idx].stable = isStable[sellToken][buyToken];
+            routes[idx].from = curSellToken;
+            routes[idx].to = curBuyToken;
+            routes[idx].stable = isStable[curSellToken][curBuyToken];
 
             unchecked {
                 ++idx;
@@ -48,23 +52,23 @@ contract LizardDex is Ownable, ILiquidityDex, LizardDexStorage {
         }
 
         if (!isFeeOnTransfer[_path[0]][_path[_path.length - 1]]) {
-            ILizardRouter01(router).swapExactTokensForTokens(
+            ILizardRouter01(Addresses.lizardRouter).swapExactTokensForTokens(
                 _sellAmount,
                 _minBuyAmount,
                 routes,
                 _receiver,
                 block.timestamp
             );
+        } else {
+            ILizardRouter01(Addresses.lizardRouter)
+                .swapExactTokensForTokensSupportingFeeOnTransferTokens(
+                    _sellAmount,
+                    _minBuyAmount,
+                    routes,
+                    _receiver,
+                    block.timestamp
+                );
         }
-
-        ILizardRouter01(router)
-            .swapExactTokensForTokensSupportingFeeOnTransferTokens(
-                _sellAmount,
-                _minBuyAmount,
-                routes,
-                _receiver,
-                block.timestamp
-            );
     }
 
     function setTokenWithFeeOnTransfer(
