26fc9f0a5e7c489f4af3c7f4c85b006e05622d83
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-05-09 00:52:53 +0800

    feat: add curve dex

--- contracts/core/dexes/CurveDex.sol
+++ contracts/core/dexes/CurveDex.sol
@@ -8,5 +8,72 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // interfaces
 import "../../interface/ILiquidityDex.sol";
+import "../../interface/curve/ICurveRegistryExchange.sol";
 
-contract CurveDex is Ownable {}
+// libraries
+import "../../libraries/Addresses.sol";
+
+// constants and types
+import {CurveDexStorage} from "../storage/CurveDex.sol";
+
+contract CurveDex is Ownable, ILiquidityDex, CurveDexStorage {
+    using SafeERC20 for IERC20;
+
+    function doSwap(
+        uint256 _sellAmount,
+        uint256 _minBuyAmount,
+        address _receiver,
+        address[] memory _path
+    ) public override returns (uint256) {
+        uint256 sellAmount = _sellAmount;
+        uint256 minBuyAmount;
+        address receiver;
+
+        for (uint256 idx; idx < _path.length - 1; ) {
+            if (idx != _path.length - 2) {
+                minBuyAmount = 1;
+                receiver = address(this);
+            } else {
+                minBuyAmount = _minBuyAmount;
+                receiver = _receiver;
+            }
+
+            address sellToken = _path[idx];
+            address buyToken = _path[idx + 1];
+            IERC20(sellToken).safeIncreaseAllowance(
+                Addresses.curveRouter,
+                sellAmount
+            );
+
+            ICurveRegistryExchange(Addresses.curveRouter).exchange(
+                _pool[sellToken][buyToken],
+                sellToken,
+                buyToken,
+                sellAmount,
+                minBuyAmount,
+                receiver
+            );
+
+            sellAmount = IERC20(buyToken).balanceOf(address(this));
+            unchecked {
+                ++idx;
+            }
+        }
+    }
+
+    function setPool(
+        address _poolAddr,
+        address _token0,
+        address _token1
+    ) public onlyOwner {
+        _pool[_token0][_token1] = _poolAddr;
+        _pool[_token1][_token0] = _poolAddr;
+    }
+
+    function getPool(
+        address _token0,
+        address _token1
+    ) public view returns (address) {
+        return _pool[_token0][_token1];
+    }
+}
--- contracts/libraries/Addresses.sol
+++ contracts/libraries/Addresses.sol
@@ -9,4 +9,5 @@ library Addresses {
         0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
     address constant camelotRouter = 0xc873fEcbd354f5A56E00E710B90EF4201db2448d;
     address constant lizardRouter = 0xF26515D5482e2C2FD237149bF6A653dA4794b3D0;
+    address constant curveRouter = 0x4c2Af2Df2a7E567B5155879720619EA06C5BB15D;
 }
