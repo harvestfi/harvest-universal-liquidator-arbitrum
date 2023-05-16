c485bb0325b6c1e731f195c6569e9dd54c5d900c
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-05-16 15:07:48 +0800

    chore: finish contract deployment scrips

---
+++ scripts/deploy-dex.ts
@@ -0,0 +1,36 @@
+import { ethers } from "hardhat";
+import { Contract, ContractFactory } from "ethers";
+import prompts from "prompts";
+
+async function main() {
+    const addrs = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", addrs[0].address);
+    console.log("Account balance:", (await addrs[0].getBalance()).toString());
+
+
+    const { dex, name } = await prompts([{
+        type: 'text',
+        name: 'dex',
+        message: 'Which dex do you want to deploy? (Ex: UniV3Dex, the contract name)',
+    },
+    {
+        type: 'text',
+        name: 'name',
+        message: 'Which name do you want to represent the dex? (Ex: uniV3)',
+    }]);
+
+    const Dex = await ethers.getContractFactory(dex) as ContractFactory & { deploy: () => Promise<Contract> };
+    if ('deploy' in Dex) {
+        const deployedDex = await Dex.deploy();
+        await deployedDex.deployed();
+        console.log(`${dex} address:`, deployedDex.address);
+    } else {
+        console.error('Dex contract factory does not have a deploy method.');
+    }
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
---
+++ scripts/deploy-ul-base.ts
@@ -0,0 +1,24 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const addrs = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", addrs[0].address);
+    console.log("Account balance:", (await addrs[0].getBalance()).toString());
+
+    const ULR = await ethers.getContractFactory("UniversalLiquidatorRegistry");
+    const registry = await ULR.deploy();
+    await registry.deployed();
+    console.log("ULR address:", registry.address);
+
+    const UL = await ethers.getContractFactory("UniversalLiquidator");
+    const universalLiquidator = await UL.deploy();
+    await universalLiquidator.deployed();
+    await universalLiquidator.setPathRegistry(registry.address);
+    console.log("UL address:", universalLiquidator.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
